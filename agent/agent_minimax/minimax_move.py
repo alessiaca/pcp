99f89d5ac38429328916bfb5d1640b41a50b4fd2
unknown <alessia.cavallo16@gmail.de>
2020-05-03 17:56:36 +0200

    Improved minimiax code

--- agent/agent_minimax/minimax_move.py
+++ agent/agent_minimax/minimax_move.py
@@ -1,11 +1,26 @@
 import numpy as np
 from typing import Optional, Tuple
-from ..connectn.common import apply_player_action
+#from ..connectn.common import apply_player_action # Fix import and use functions from common.py
 
 
 PlayerAction = np.int8
 BoardPiece = np.int8
 SavedState = np.int8
+
+
+def apply_player_action(
+        board: np.ndarray, action: PlayerAction, player: BoardPiece, copy: bool = False) \
+        -> np.ndarray:
+    """
+    :param board: State of board, 6 x 7 with either 0 or player ID [1, 2]
+    :param action: Column where player should be dropped
+    :param player: player ID [1, 2]
+    :param copy:
+    :return: New state of board after player was dropped in column
+    """
+    max_free_ind = np.max(np.where(board[:, action] == 0))
+    board[max_free_ind, action] = player
+    return board
 
 
 def eval_board(board: np.ndarray, player: BoardPiece) -> int:
@@ -41,13 +56,12 @@ def eval_board(board: np.ndarray, player: BoardPiece) -> int:
                 counter = 0
                 for j, i in enumerate(range(row_i, n_rows-1)):
                     counter = compare_states_and_count(board[i, j], board[i+1, j+1], player, counter)
-                if counter == 3:
                 max_counter = max(counter, max_counter)
 
     return max_counter
 
 
-def minimax(board: np.adarray, player: BoardPiece, depth: int, MaxPlayer: bool) \
+def minimax(board: np.ndarray, player: BoardPiece, depth: int, MaxPlayer: bool) \
         -> Tuple[PlayerAction, np.ndarray]:
     if depth == 0:
         return 0, eval_board(board, player)
@@ -66,8 +80,7 @@ def minimax(board: np.adarray, player: BoardPiece, depth: int, MaxPlayer: bool) 
         for action, column in enumerate(board):
             if 0 in column:
                 board = apply_player_action(board, action, player)
-                counter = minimax(board, player, depth - 1, True)
-                min_counter = min(min_counter, counter)
+                action, counter = minimax(board, player, depth - 1, True)
                 if counter <= min_counter:
                     min_counter = counter
                     min_action = action
